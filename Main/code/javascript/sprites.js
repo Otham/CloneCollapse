3e3e7f2a19504745f9cc73f7ef6c284bd8db7045
Jonathan Murfey <jonathanmurfey@hotmail.com>
2014-07-05 13:15:27 -0500

    directory structure and library file
    fixed up directory layout and added a sound library I found on the net
    which might be useful

---
+++ Main/code/javascript/sprites.js
@@ -0,0 +1,18 @@
+	function createSprite(locY, locX) {
+		var sp = new Object();					
+		var circle = new createjs.Shape();
+		var color = parseInt(Math.random() * 6);
+		circle.graphics.beginStroke("black").beginFill(spriteColors[color]).drawCircle(0, 0, 25);
+						
+		circle.x = cells[locY][locX].x;
+		circle.y = cells[locY][locX].y;
+		circle.name = parseInt(Math.random() * 10000000);
+		sp.circle = circle;
+		sp.active = false;
+		sp.cellC = 0;
+		sp.cellR = 0;
+		sp.lifeTime = 0;
+		sp.value = color;
+		stage.addChild(circle);
+		return sp;
+	}
