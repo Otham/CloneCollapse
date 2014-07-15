710d433f780b93b3670d573e1c8026c0e15d9999
Puru <otham77@gmail.com>
2014-07-15 17:25:56 -0500

    Toggle background Music
    added toggle sound on/off button to game.html.
    Add bgmusic.js file.
    Added bigbounce.wav file to sounds file

---
+++ Main/code/javascript/bgmusic.js
@@ -0,0 +1,22 @@
+﻿//Background Music
+var music = 0;	
+
+
+function playMusic()
+{
+	document.getElementById('bgMusic').play();
+
+}
+
+function pauseMusic()
+{
+		music = 1-music;
+		
+		if (music == 1)
+			document.getElementById('bgMusic').pause();
+		else
+			playMusic();
+
+}
+
+
